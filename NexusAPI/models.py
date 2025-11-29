6a4c72a8ba8f1bd3fcc9dde056d0cf7d727e2063
code5mith <jacobscontact1@gmail.com>
2025-11-29 15:37:14 -0500

    Refactor(product) : update product data model

--- NexusAPI/models.py
+++ NexusAPI/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     slug = models.SlugField()
     image = models.ImageField(upload_to="product_image", blank=True, null=True)
+    featured = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name="products")
 
     def __str__(self) -> str:
