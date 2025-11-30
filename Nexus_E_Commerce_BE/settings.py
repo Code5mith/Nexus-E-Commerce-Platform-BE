dde5fa4ba02ccd077101b8fc15c4a80d03e4607c
code5mith <jacobscontact1@gmail.com>
2025-11-30 15:13:16 -0500

    Feat : config debug

--- Nexus_E_Commerce_BE/settings.py
+++ Nexus_E_Commerce_BE/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-auehl4+v&i%v!*8y9dfvhdn-7ky28n78ehcyn0hd7sl%v2@xwv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
